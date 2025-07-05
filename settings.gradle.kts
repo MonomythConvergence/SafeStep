65561682bd83ed73d6dae4c18933450ec074eec1
Monomythconvergence <shoulddoisuppose@gmail.com>
2025-07-05 20:14:54 +0300

    Project setup start

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SafeStep"
+include(":app")
+include(":core")
+include(":data")
+include(":features")
